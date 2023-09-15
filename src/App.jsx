ffa7a821e576fa5de8068f569e450242b567402f
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-09-15 22:51:05 +0530

    feat - added routing for cart and about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
-import ProductCard from './components/ProductCard'
-
+import AboutUs from './components/AboutUs'
+import Cart from './components/Cart'
 import './App.css'
+
+import { createBrowserRouter, Outlet } from 'react-router-dom'
 
 function App() {
 
@@ -13,11 +15,35 @@ function App() {
 
       {/* <ProductCard /> */}
       <Header />
-      <Body />
+      <Outlet />
       <Footer />
 
     </>
   )
 }
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+
+    ],
+  },
+]);
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import pardaLogo from '../assets/prada-logo.png';
 import Cart from './Cart';
 import AboutUs from './AboutUs'
+import { Link } from 'react-router-dom';
 const Logo = () => (
-    <img className='w-1/1 h-1/2 ml-4'
-        src={pardaLogo}
-        alt='PardaLogo'
-        title='Parda Logo'
-    />
+    <a href='/'>
+        <img className='w-1/1 h-1/2 ml-4'
+            src={pardaLogo}
+            alt='PardaLogo'
+            title='Parda Logo'
+        />
+    </a>
 
 
 );
@@ -19,8 +22,13 @@ function Header() {
             <div className=" h-24 w-auto bg-amber-900 flex justify-between ">
                 <Logo />
                 <div className=' flex gap-4 '>
-                    <AboutUs />
-                    <Cart />
+                    <div>
+                        <Link to="/about">About</Link>
+                    </div>
+                    <div>
+                        <Link to="/cart">Cart</Link>
+                    </div>
+
                 </div>
 
             </div>
