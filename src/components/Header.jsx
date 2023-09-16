245f43b79f681b01349c7dc54146a520d1221f50
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-09-16 20:23:33 +0530

    style(productCard)-layout design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
-import pardaLogo from '../assets/prada-logo.png';
-import Cart from './Cart';
-import AboutUs from './AboutUs'
+import hannahLogo from '../assets/dummy-png.png';
+import shoppingCart from '../assets/shopping_cart.svg'
 import { Link } from 'react-router-dom';
 const Logo = () => (
     <Link to='/'>
 
         <img className='w-1/1 h-1/2 ml-4 mt-4'
-            src={pardaLogo}
-            alt='PardaLogo'
-            title='Parda Logo'
+            src={hannahLogo}
+            alt='hannahLogo'
+            title='Hannah Logo'
         />
     </Link>
 
@@ -23,12 +22,13 @@ function Header() {
             <div className=" h-24 w-auto bg-custom-purple flex justify-between">
                 <Logo />
                 <div className=' flex gap-4 mt-4'>
-                    <div>
+                    <div  >
+                        <Link to="/cart"><img src={shoppingCart} alt="cart" /> </Link>
+                    </div>
+                    <div className='mr-4  '>
                         <Link to="/about">About</Link>
                     </div>
-                    <div className='mr-4  ' >
-                        <Link to="/cart">Cart</Link>
-                    </div>
+
 
                 </div>
 
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,46 @@
+import { products } from "../constants";
 function ProductCard() {
     return (
         <>
             <div className="flex flex-wrap justify-center " >
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 1</div>
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 2</div>
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 3</div>
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 4</div>
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 5</div>
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 6</div>
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 7</div>
-                <div className=" h-80 w-80 m-1 rounded-lg drop-shadow-xl border-solid border-2 border-purple-500 bg-eye-protect" >card 8</div>
+                <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
+                    {products.map((product) => (
+                        <div key={product.id} className="rounded-md border">
+                            <img
+                                src={product?.imageSrc}
+                                alt="Laptop"
+                                className="aspect-[16/9] w-full rounded-md md:aspect-auto md:h-[300px] lg:h-[200px]"
+                            />
+                            <div className="p-4">
+                                <h1 className="inline-flex items-center text-lg font-semibold">{product?.name}</h1>
+                                <p className="mt-4 font-semibold">{product?.price}</p>
+                                <p className="mt-3 text-sm text-gray-600">
+                                    {product?.description}
+                                </p>
+
+                                <div className="mt-5 flex items-center space-x-2">
+                                    <span className="block text-sm font-semibold">Size : </span>
+                                    <span className="block cursor-pointer rounded-md border border-gray-300 p-1 px-2 text-xs font-medium">
+                                        {product?.allSize?.size1}
+                                    </span>
+                                    <span className="block cursor-pointer rounded-md border border-gray-300 p-1 px-2 text-xs font-medium">
+                                        {product?.allSize?.size2}
+                                    </span>
+                                    <span className="block cursor-pointer rounded-md border border-gray-300 p-1 px-2 text-xs font-medium">
+                                        {product?.allSize?.size3}
+                                    </span>
+                                </div>
+                                <button
+                                    type="button"
+                                    className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                                >
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
             </div>
         </>
     )
