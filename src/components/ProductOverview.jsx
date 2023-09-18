cf94ec0d99d32692eb9932a4c2647e76e0f041e0
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-09-18 22:05:51 +0530

    style-inhanced design of ProductOverview component

--- src/components/ProductOverview.jsx
+++ src/components/ProductOverview.jsx
@@ -3,8 +3,8 @@ function ProductOverview() {
     return (
         <>
             <div className="flex justify-center">
-                <div className="  w-1/2 h-1/2 flex flex-wrap justify-center gap-2  ">
-                    <img className="w-64 rounded-lg " src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/21470290/2023/1/9/ef2cc4b3-5ec5-4e3d-93d6-85a70475f97c1673260187429RelaxedFitHoodie1.jpg" alt="" />
+                <div className="  w-1/2 flex justify-center ">
+                    <img className="w-68 rounded-lg" src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/21470290/2023/1/9/ef2cc4b3-5ec5-4e3d-93d6-85a70475f97c1673260187429RelaxedFitHoodie1.jpg" alt="" />
                 </div>
                 <div className="  w-1/2">
                     <h1 className="mb-1 font-extrabold text-xl">Nike Air Max 95 By You</h1>
@@ -16,19 +16,19 @@ function ProductOverview() {
                     <div className="border-solid border-t border-gray-300 h-16">
                         <h1 className="mt-1 ml-1 font-bold">Size: </h1>
                         <div className="flex gap-2">
-                            <p className="border-solid font-medium border border-gray-300 mb-1 ml-1">S</p>
-                            <p className="border-solid font-medium border border-gray-300 mb-1">M</p>
-                            <p className="border-solid font-medium border border-gray-300 mb-1">XL</p>
+                            <p className="border-solid font-medium border border-gray-300 mb-1 ml-1 cursor-pointer p-1">S</p>
+                            <p className="border-solid font-medium border border-gray-300 mb-1 cursor-pointer p-1">M</p>
+                            <p className="border-solid font-medium border border-gray-300 mb-1 cursor-pointer p-1">XL</p>
                         </div>
 
                     </div>
                     <div className="border-solid border-t border-gray-300 flex justify-between h-12">
                         <div className="border-solid border border-gray-300 mt-1 ml-1 font-medium flex w-48 justify-center rounded-md">
-                            <div className="border-solid border-r border-gray-300 p-1 w-10 text-center">+</div>
-                            <div className="border-solid border-r border-gray-300 p-1 w-10 text-center">1</div>
-                            <div className="border-solid p-1 w-10 text-center">-</div>
+                            <div className="border-solid border-r border-gray-300 p-1 w-10 text-center cursor-pointer">+</div>
+                            <div className="border-solid border-r border-gray-300 p-1 w-10 text-center cursor-pointer">1</div>
+                            <div className="border-solid p-1 w-10 text-center cursor-pointer">-</div>
                         </div>
-                        <div className=" bg-black text-white mt-1 mb-1 mr-1 font-medium rounded-md p-1 w-48 text-center">Add to cart</div>
+                        <div className=" bg-black text-white mt-1 mb-1 mr-1 font-medium rounded-md p-1 w-48 text-center cursor-pointer">Add to cart</div>
                     </div>
                     <div className="border-solid border-t border-gray-300 mt-1 mb-1 font-medium">SKU: N/A
 
